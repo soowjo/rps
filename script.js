c1f9c472aa383a3cfec2683ea81e6af6ad86babc
Soo <soowjo@gmail.com>
2022-07-04 15:24:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function computerPlay {
+function computerPlay() {
     let ranNum3 = Math.floor(Math.random()*3)
     if (ranNum3 == 0) {
         return 'rock';
@@ -8,6 +8,7 @@ function computerPlay {
     }
     else if (ranNum3 == 2) {
         return 'scissors';
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -15,24 +16,38 @@ function playRound(playerSelection, computerSelection) {
         case (playerSelection == 'rock' && computerSelection == 'scissors'):
         case (playerSelection == 'paper' && computerSelection == 'rock'):
         case (playerSelection == 'scissors' && computerSelection == 'paper'):
-            return 'player'
-
+            console.log("You Win!")
+            return 'player';
         case (playerSelection == 'scissors' && computerSelection == 'rock'):
         case (playerSelection == 'rock' && computerSelection == 'paper'):
         case (playerSelection == 'paper' && computerSelection == 'scissors'):
-            return 'computer'
+            console.log("You Lose!")
+            return 'computer';
         case (playerSelection == computerSelection):
-            return 'even'
-
+            console.log("Tie"!)
+            return 'even';
     }
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
-        (playerSelection == 'paper' && computerSelection == 'rock') || 
-        (playerSelection == 'scissors' && computerSelection == 'paper')) {
-            return 'player'
+}
+
+function game() {
+    for (let i=0; i<5; i++) {
+        let playerScore = 0;
+        let computerScore = 0;
+        let player;
+        do {
+            player = prompt('Enter either rock, paper, or scissors').toLowerCase();
+        } while (!(player == 'rock' || player == 'paper' || player == 'scissors'));
+        let computer = computerPlay();
+        switch (playRound(player, computer)) {
+            case 'player':
+                playerScore += 1;
+                break;
+            case 'computer':
+                computerScore += 1;
+                break;
+            case 'even':
+                break;
         }
-    else if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
-        (playerSelection == 'paper' && computerSelection == 'rock') || 
-        (playerSelection == 'scissors' && computerSelection == 'paper')) {
-            return 'player'
-        }
-}   
+        console.log(i)
+    }
+}
